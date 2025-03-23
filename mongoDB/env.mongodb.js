ddda2a8762e45f7606de8f979892d41ab0e68b04
Hussain <hedayati1488@gmail.com>
2025-03-23 14:25:52 +0330

    ksdj

---
+++ mongoDB/env.mongodb.js
@@ -0,0 +1,129 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+const database = 'my_app';
+const userActivitycollection = 'user_activity';
+const reviews = 'reviews';
+const cart = 'cart';
+const wishlist = 'wishlist';
+
+const product_table = 'products';
+const user_table = 'users';
+
+
+// The current database to use.
+use(database);
+
+// Defining the required format for user activiy collection.
+db.createCollection(userActivitycollection, {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ['user_id', 'logs'],
+      properties: {
+        user_id: { bsonType: 'int' },
+        logs: {
+          bsonType: 'array',
+          logs: {
+            bsonType: "object",
+            required: ['date', 'activity'],
+            properties: {
+              date: { bsonType: 'date' },
+              activity: { bsonType: 'string' }
+            }
+          }
+        }
+      },
+    }
+  }
+});
+
+// Defining the required format for review Collection.
+db.createCollection(reviews, {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["product_id", "review"],
+      properties: {
+        product_id: { bsonType: "int" },
+        reviews: {
+          bsonType: "array",
+          items: {
+            bsonType: "object",
+            required: ['id', 'user_id', 'message'],
+            properties: {
+              id: { bsonType: 'objectId' },
+              user_id: { bsonType: 'int' },
+              message: { bsonType: 'string' }
+            }
+          }
+        }
+      }
+    }
+  }
+});
+
+
+// Defining the required format for cart collection.
+db.createCollection(cart, {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["user_id", "items"],
+      properties: {
+        user_id: { bsonType: "int" },
+        items: {
+          bsonType: "array",
+          items: {
+            bsonType: "object",
+            required: ["product_id", "quantity", "created_at"],
+            properties: {
+              product_id: { bsonType: "int" },
+              quantity: { bsonType: "int" },
+              created_at: { bsonType: "date" }
+            }
+          }
+        }
+      }
+    }
+  }
+});
+
+// Difining the reqiered format for user wish list collection.
+db.createCollection(wishlist, {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["user_id", "items"],
+      properties: {
+        user_id: { bsonType: 'int' },
+        items: {
+          bsonType: "array",
+          items: {
+            bsonType: "object",
+            required: ["product_id", "added_at"],
+            product_id: { bsonType: 'int' },
+            added_at: { bsonType: "date" }
+          }
+        },
+      }
+    }
+  }
+});
+
+// Difinitioin of collections indexes:
+
+db.userActivitycollection.createIndex({ user_id: 1 });
+
+db.reviews.createIndex({ product_id: 1, "items.user_id": 1 });
+
+db.cart.createIndex({ user_id: 1 });
+
+db.wishlist.createIndex({ user_id: 1 });
+
+
+// Creating sql tables, sample for testing
+
+db.createCollection(user_table);
+db.createCollection(product_table)
